16ea6aa6a159ddd046e581b32a24d71abdc51d72
anasattili <anasattily@yahoo.com>
2020-05-20 00:27:17 +0300

    save changes

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 
 var Seattle= {
     name: 'Seattle',
-    times: ['1amp','6am:','7am:',' 8am:','9am:','10am:','11am:','12pm:','1pm:','2pm:','3pm:','4pm:','5pm:',' 6pm:',' 7pm:'],
+    times: ['6am:','7am:',' 8am:','9am:','10am:','11am:','12pm:','1pm:','2pm:','3pm:','4pm:','5pm:',' 6pm:',' 7pm:'],
     sum:0,        
     ranNum:0,
     
