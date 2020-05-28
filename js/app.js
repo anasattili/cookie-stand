ca854d2f8fd2dbde7d9d0dc2f63d263f3b723349
anasattili <anasattily@yahoo.com>
2020-05-29 02:52:05 +0300

    finish salmon cookies

--- js/app.js
+++ js/app.js
@@ -1,12 +1,13 @@
 'use strict'
 
 
-var times = ['6:am','7:am','8:am','9:am','10:am','11:am','12:pm','1:pm','2:pm','3:pm','4:pm','5:pm','6:pm','7:pm',] ; 
+var times = ['6:am','7:am','8:am','9:am','10:am','11:am','12:pm','1:pm','2:pm','3:pm','4:pm','5:pm','6:pm','7:pm','The sum of Daily Location Total'];
 
 times.unshift('Locations ');
-times.push('The sum of Daily Location Total');
+//times.push('The sum of Daily Location Total');
 
 var locations = [];
+
 
 function Cities(name, customerMin, customerMax, avgCookies) {
   this.name = name;
@@ -14,7 +15,7 @@ function Cities(name, customerMin, customerMax, avgCookies) {
   this.customerMax = customerMax;
   this.avgCookies = avgCookies;
   this.totalCookiesNum = 0;
-  this.cusEachHour = [];
+   this.cusEachHour = [];
   this.avgEachHour = [];
   locations.push(this);
 }
@@ -40,13 +41,17 @@ Cities.prototype.avgHour = function () {
 
   for (var i = 0; i < this.cusEachHour.length - 2; i++) {
 
-    var avgOfCooki = Math.round(this.cusEachHour[i] * this.avgCookies)
+    var avgOfCooki = Math.round(this.cusEachHour[i] * this.avgCookies);
 
     this.avgEachHour.push(avgOfCooki);
     this.totalCookiesNum += this.avgEachHour[i];
   }
-
+  
+ 
+  
+ 
 };
+
 
 var container = document.getElementById('sales');
 var table1 = document.createElement('table');
@@ -72,13 +77,39 @@ Cities.prototype.render = function () {
   }
 };
 
-var salesForm=document.getElementById('salesfrom')
+
+
+  var salesForm=document.getElementById('salesfrom');
 salesForm.addEventListener('submit', function(event){
   event.preventDefault();
-  console.log(event);
-  /* var shopName= event.target.shopName;
-  console.log(shopName); */
+  
+   var shopName= event.target.ShopName.value;
+  console.log(shopName); 
+
+  var MinHourlyCustomer= event.target.MinHourlyCustomer.value;
+  console.log(MinHourlyCustomer); 
+
+  var MaxHourlyCustomer= event.target.MaxHourlyCustomer.value;
+  console.log(MaxHourlyCustomer); 
+
+  var AverageCookies= event.target.AverageCookies.value;
+  console.log(AverageCookies); 
+
+  var newShops = new Cities(shopName, parseInt(MinHourlyCustomer), parseInt(MaxHourlyCustomer), parseInt(AverageCookies));
+
+  newShops.getCusEachHour();
+  newShops.avgHour();
+
+  table1.removeChild(table1.lastChild);
+  newShops.render();
+  rendTheLastRow();
+
+
+
+
 });
+
+
 //for the first row (head table)
 function rendOneRow() {
 
@@ -89,7 +120,7 @@ function rendOneRow() {
     th1.textContent = times[i];
 
   }
-}
+} 
 
 // for the totall row
 function rendTheLastRow() {
@@ -104,20 +135,29 @@ function rendTheLastRow() {
 
   var timesTotal = [];
 
-  for (var j = 0; j < times.length - 2; j++) {
+  for (var j = 0; j < times.length-2 ; j++) {
     var totalElement = 0;
     for (var i = 0; i < locations.length; i++) {
       var elementsOfColoumn = locations[i].avgEachHour[j];
       totalElement += elementsOfColoumn;
-      
-    }
+}
 
-    timesTotal.push(totalElement);
-    
+ timesTotal.push(totalElement);
+
+        var theTotalSumOfDays=0;
+for(var i=0 ;i<timesTotal.length;i++)
+{
+  theTotalSumOfDays +=timesTotal[i];
+
+}
+
   }
-  
+  timesTotal.push(theTotalSumOfDays);
+  console.log(timesTotal[14]);
+  console.log(theTotalSumOfDays);
 
-  for (var i = 0; i < times.length - 2; i++) {
+  for (var i = 0; i < times.length-1 ; i++) {
+
     var totalRowElement = document.createElement('td');
     totRow.appendChild(totalRowElement);
     totalRowElement.textContent = timesTotal[i];
@@ -129,13 +169,13 @@ function rendTheLastRow() {
 
 function rendAll() {
 
- 
+  
   rendOneRow();
 
   for (var i = 0; i < locations.length; i++) {
     locations[i].render();
   }
-
+  
   rendTheLastRow();
 }
 
@@ -144,14 +184,13 @@ function rendAll() {
 for (var i = 0; i < locations.length; i++) {
   locations[i].getCusEachHour();
   locations[i].avgHour();
-  
+
 }
 
 rendAll();
 
 
 
-  
 
 
 
